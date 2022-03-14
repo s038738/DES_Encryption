a3a9a412bdd13cdd6f65283c6d5c0b3f0b95f975
s038738 <s038738@ad.viko.lt>
2022-03-14 13:14:36 +0200

    Simple encryption and decryption

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,69 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
 public class Main {
 
+    public static void main(String[] args) {
+        try {
+            String key = "squirrel123"; // needs to be at least 8 characters for DES
 
-    public static void main(String[] args){
+            FileInputStream fis = new FileInputStream("D:\\VIKO\\4sem\\IS\\2\\src\\original.txt");
+            FileOutputStream fos = new FileOutputStream("D:\\VIKO\\4sem\\IS\\2\\src\\encrypted.txt");
+            encrypt(key, fis, fos);
 
-
-
-        
+            FileInputStream fis2 = new FileInputStream("D:\\VIKO\\4sem\\IS\\2\\src\\encrypted.txt");
+            FileOutputStream fos2 = new FileOutputStream("D:\\VIKO\\4sem\\IS\\2\\src\\decrypted.txt");
+            decrypt(key, fis2, fos2);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 
+    public static void encrypt(String key, InputStream is, OutputStream os) throws Throwable {
+        encryptOrDecrypt(key, Cipher.ENCRYPT_MODE, is, os);
+    }
+
+    public static void decrypt(String key, InputStream is, OutputStream os) throws Throwable {
+        encryptOrDecrypt(key, Cipher.DECRYPT_MODE, is, os);
+    }
+
+    public static void encryptOrDecrypt(String key, int mode, InputStream is, OutputStream os) throws Throwable {
+
+        DESKeySpec dks = new DESKeySpec(key.getBytes());
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
+        SecretKey desKey = skf.generateSecret(dks);
+        Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding"); // DES/ECB/PKCS5Padding for SunJCE
+
+        if (mode == Cipher.ENCRYPT_MODE) {
+            cipher.init(Cipher.ENCRYPT_MODE, desKey);
+            CipherInputStream cis = new CipherInputStream(is, cipher);
+            doCopy(cis, os);
+        } else if (mode == Cipher.DECRYPT_MODE) {
+            cipher.init(Cipher.DECRYPT_MODE, desKey);
+            CipherOutputStream cos = new CipherOutputStream(os, cipher);
+            doCopy(is, cos);
+        }
+    }
+
+    public static void doCopy(InputStream is, OutputStream os) throws IOException {
+        byte[] bytes = new byte[64];
+        int numBytes;
+        while ((numBytes = is.read(bytes)) != -1) {
+            os.write(bytes, 0, numBytes);
+        }
+        os.flush();
+        os.close();
+        is.close();
+    }
 
 }
